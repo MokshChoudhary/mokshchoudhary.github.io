0459237e5922e49945e388f819f23d2c589074f0
MokshChoudhary <moksh.choudhary.2@google.com>
2024-03-25 03:35:51 +0530

    add update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
 import './App.css'
 import menu from "./assets/menu.png"
 import Card from './comp/Card'
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
 
   return (
-    <div className='text-[#000] w-screen h-screen p-0 m-0 '>
-      <header className='fixed h-28 bg-white top-0 z-10  w-screen border-b-2 border-x-black shadow-sm '>
+    <div className='text-[#000] w-screen h-screen p-0 m-0'>
+      <div className=' hidden fixed bg-secondary-color top-0 left-0 h-dvh w-screen z-10 overflow-hidden'>
+        <Nav className=" max-md:hidden absolute transform -translate-y-1/2 top-1/2 right-2 grid grid-flow-col">
+          <Nav.Link className='mx-3 font-thin' href="#aboutMe">ABOUT ME</Nav.Link>
+          <Nav.Link className='mx-3 font-thin' href="#resume">RESUME</Nav.Link>
+          <Nav.Link className='mx-3 font-thin' href="#project">PROJECT</Nav.Link>
+          <Nav.Link className='mx-3 font-thin' href="#contact">CONTACT</Nav.Link>
+        </Nav>
+      </div>
+
+
+      <header className='max:md:fixed max-md:h-1/2 md:h-28 bg-white top-0 max-md:z-10  w-screen border-b-2 border-x-black shadow-sm '>
         <nav className='h-full md:grid max-md:flex grid-cols-2 gap-2'>
           <div className='relative h-full w-full'>
-            <div className='absolute  transform -translate-y-1/2 top-1/2 left-2'>
-              <div className='h-[15px] w-[15px] inline-flex bg-primary-button-color' />
-              <div className='inline text-2xl font-bold'>Moksh Choudhary</div>  <div className='max-md:hidden m-0 p-0 left-0 font-mono inline'>Software Engineer</div>
+            <div className='absolute  md:transform md:-translate-y-1/2 md:top-1/2 md:left-2 max-md:top-3 max-md:left-3'>
+              <div className='h-[15px] w-[15px] me-2 inline-flex bg-primary-button-color' />
+              <div className='inline text-2xl font-bold'>Moksh Choudhary</div>  <div className=' m-0 p-0 left-0 font-mono inline'><br className='md:hidden' />Software Engineer</div>
             </div>
           </div>
-          <div className='relative h-full'>
-            <div className='md:hidden'>
-              <img className='h-8 absolute transform -translate-y-1/2 top-1/2 right-2' src={String(menu)}></img>
+          <div className='md:relative '>
+            <div className='md:hidden absolute top-6 right-5'>
+              <img className='h-8 w-8' src={String(menu)} />
+
             </div>
-            <div className='max-md:hidden absolute transform -translate-y-1/2 top-1/2 right-2 grid grid-flow-col'>
-              <div className='mx-3 font-thin active'>ABOUT ME</div>
-              <div className='mx-3 font-thin'>RESUME</div>
-              <div className='mx-3 font-thin'>PROJECT</div>
-              <div className='mx-3 font-thin'>CONTACT</div>
-            </div>
+            <Nav className="me-auto max-md:hidden absolute transform -translate-y-1/2 top-1/2 right-2 grid grid-flow-col">
+              <Nav.Link className='mx-3 font-thin' href="#aboutMe">ABOUT ME</Nav.Link>
+              <Nav.Link className='mx-3 font-thin' href="#resume">RESUME</Nav.Link>
+              <Nav.Link className='mx-3 font-thin' href="#project">PROJECT</Nav.Link>
+              <Nav.Link className='mx-3 font-thin' href="#contact">CONTACT</Nav.Link>
+            </Nav>
           </div>
         </nav>
       </header>
-      <div className=' bg-white top-28 w-screen h-screen bg-primary-color grid-flow-col'>
+      <div className=' bg-white top-28 w-screen h-1/2 md:h-screen bg-primary-color grid-flow-col'>
 
-        <div className='md:bg-primary-color h-screen w-[40%]' />
+        <div className='max-md:absolute max-md:top-[40%] bg-primary-color h-screen w-screen md:w-[40%]' />
       </div>
 
       <Card />
--- src/comp/Card.tsx
+++ src/comp/Card.tsx
@@ -6,31 +6,35 @@ import linkdin from "../assets/linkedin.png"
 export default function Card() {
     return (
         <>
-            <div className="absolute max-md:w-screen md:w-[70%] md:top-[25%] md:left-[20%] max-md:top-28">
+            <div className="absolute w-[70%] md:top-[25%] left-[15%] md:left-[25%] max-md:top-28">
                 <div className="md:flex">
                     <div className="bg-secondary-color max-md:mb-10 w-full border shadow-lg">
-                        <img className="rounded-full w-60 md:p-8 max-md:p-1 m-auto" src={String(moksh)} alt="Moksh Choudhary" />
+                        <img className="rounded-full w-60 p-8 m-auto" src={String(moksh)} alt="Moksh Choudhary" />
                         <div className="">
-                            <div className="font-bold text-3xl my-6 text-center">Moksh <br className="max-md:hidden"/> Choudhary</div>
+                            <div className="font-bold text-3xl my-6 text-center">Moksh <br className="max-md:hidden" /> Choudhary</div>
                             <div className='bg-primary-button-color w-20 my-2 h-[2px] mx-auto' />
-                            <div className="text-[#1a1919] my-6 tracking-widest text-xl font-thin text-center mx-auto">
+                            <div className="text-[#1a1919] max-md:hidden my-6 tracking-widest text-xl font-thin text-center mx-auto">
                                 SOFTWARE ENGINEER
+                            </div>
+                            <div className="flex md:hidden justify-center items-center my-6">
+                                <button className="bg-primary-button-color transition-colors duration-300 ease-in-out hover:bg-white hover:border-primary-button-color border-2 hover:text-black rounded-full px-6 py-1 mx-3 border-primary-button-color text-white">Resume</button>
+                                <button className="bg-white transition-colors duration-300 ease-in-out hover:bg-primary-button-color hover:border-primary-button-color border-2 hover:text-white rounded-full px-6 py-1 border-black">Project</button>
                             </div>
                         </div>
                         <div className="relative bottom-0 bg-white">
                             <div className="flex items-center justify-center">
-                                <img src={String(git)}     className="w-6 mx-6 py-2"/>
-                                <img src={String(linkdin)} className="w-6 mx-6 py-2"/>
-                                <img src={String(insta)}   className="w-6 mx-6 py-2"/>
+                                <img src={String(git)} className="w-6 mx-6 py-2" />
+                                <img src={String(linkdin)} className="w-6 mx-6 py-2" />
+                                <img src={String(insta)} className="w-6 mx-6 py-2" />
                             </div>
                         </div>
                     </div>
-                    <div className="ms-10">
+                    <div className="md:ms-10 mt-10">
                         <h1 className="text-8xl font-bold">Hello </h1>
-                        <h1 className="text-2xl">Here's who I am & what I do </h1>
-                        <div className="flex my-6">
-                            <button className="rounded-full px-6 py-1 mx-3 bg-primary-button-color text-white">Resume</button>
-                            <button className="rounded-full px-6 py-1 border border-black">Project</button>
+                        <h1 className="text-2xl mb-6">Here's who I am & what I do </h1>
+                        <div className="flex max-md:hidden  my-6">
+                            <button className="bg-primary-button-color transition-colors duration-300 ease-in-out hover:bg-white hover:border-primary-button-color border-2 hover:text-black rounded-full px-10 py-1 mx-1 border-primary-button-color text-white">Resume</button>
+                            <button className="bg-white transition-colors duration-300 ease-in-out hover:bg-primary-button-color hover:border-primary-button-color border-2 hover:text-white rounded-full px-10 py-1 border-black">Project</button>
                         </div>
                         <div>
                             I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.
