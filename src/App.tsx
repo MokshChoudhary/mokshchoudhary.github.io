afc19e3b9724b2706cb4740bc3e8c360ad039e8f
MokshChoudhary <moksh.choudhary.2@google.com>
2024-03-25 01:50:55 +0530

    new web

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import menu from "./assets/menu.png"
 import Card from './comp/Card'
-import NameCard from './comp/NameCard'
 
 function App() {
 
@@ -31,9 +30,6 @@ function App() {
       <div className=' bg-white top-28 w-screen h-screen bg-primary-color grid-flow-col'>
 
         <div className='md:bg-primary-color h-screen w-[40%]' />
-
-        {/* <NameCard classNameString={"mx-auto h-[60%] text-center absolute text-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"} /> */}
-
       </div>
 
       <Card />
